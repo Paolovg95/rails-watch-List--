ad703fbef23acb158b4b0f050337f7db502301e3
Paolo Vargas <paolo9517@gmail.com>
2021-10-18 20:01:39 -0300

    Delete Bookmark added

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,5 @@
 class BookmarksController < ApplicationController
+  before_action :find_bookmark, only: [:destroy]
   before_action :find_list, only: [:new,:create]
   def new
     @bookmark = Bookmark.new
@@ -14,10 +15,19 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
   private
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def find_bookmark
+    @bookmark = Bookmark.find(params[:id])
   end
 
   def find_list
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
   resources  :lists, only: [:new, :create, :show]  do
-    resources :bookmarks, only: [:new, :create, :delete]
+    resources :bookmarks, only: [:new, :create]
   end
+
+  resources :bookmarks, only: [:destroy]
 end
