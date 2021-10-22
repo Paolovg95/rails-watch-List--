6a75e888807496c3842ac374324eacf4d47c89fd
Paolo Vargas <paolo9517@gmail.com>
2021-10-21 21:20:04 -0300

    Clodunary images

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+  has_many_attached :photos
 
   validates :comment, length: { minimum: 6 }
   validates :movie_id, uniqueness: { scope: :list_id }
